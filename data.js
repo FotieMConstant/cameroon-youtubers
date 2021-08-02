cbef496ef5837511dfbbf7f245ae90c9efb8cfa9
fotiecodes <42372656+FotieMConstant@users.noreply.github.com>
2021-08-02 10:44:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,27 @@
-
+// The array of youtubers
+module.exports = [
+  {
+    id: "0",
+    name: "CodewithBro",
+    owner: "CodewithBro Team",
+    approxSubs: "220+",
+    type: "Tech",
+    tags: [
+    "Technology",
+    "Coding"
+    ],
+    author: "Unknown Author",
+  },
+  {
+    id: "0",
+    name: "CodewithBro",
+    owner: "CodewithBro Team",
+    approxSubs: "220+",
+    type: "Tech",
+    tags: [
+    "Technology",
+    "Coding"
+    ],
+    author: "Unknown Author",
+  },
+];
